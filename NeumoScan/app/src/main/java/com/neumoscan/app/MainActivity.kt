37a7c912096a9f79e6a92e909952dbf32e97c736
William Eckerleben <william.eckerleben@nts.com.py>
2023-05-30 20:46:46 -0400

    take picture and display it

--- NeumoScan/app/src/main/java/com/neumoscan/app/MainActivity.kt
+++ NeumoScan/app/src/main/java/com/neumoscan/app/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.neumoscan.app
 
+import androidx.compose.foundation.layout.Box
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Bundle
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -34,6 +42,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.neumoscan.app.ui.theme.NeumoScanTheme
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.util.Date
 import java.util.Locale
@@ -42,13 +51,12 @@ import java.util.Locale
 class MainActivity : ComponentActivity() {
     private companion object {
         private const val REQUEST_IMAGE_CAPTURE = 1
-        private const val REQUEST_CAMERA_PERMISSION = 2
-        private const val CAMERA_PERMISSION_CODE = 100
         private const val PERMISSION_REQUEST_CODE = 1001
     }
 
-    private val inputSize: Long = 224L
     private var currentPhotoPath: String? = null
+    private lateinit var imageView: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,20 +66,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
+                    Box(
                         modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        contentAlignment = Alignment.Center
                     ) {
-                        Greeting("Android")
-                        Spacer(modifier = Modifier.height(16.dp))
-                        TakePhotoButton(this@MainActivity)
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Greeting("Android")
+                            Spacer(modifier = Modifier.height(16.dp))
+                            TakePhotoButton(this@MainActivity)
+                        }
+                        if (currentPhotoPath != null) {
+                            ShowPhoto(currentPhotoPath!!)
+                        }
                     }
                 }
             }
         }
+        checkAndRequestPermissions()
     }
-
 
     private fun checkAndRequestPermissions() {
         val permissionsToRequest = mutableListOf<String>()
@@ -90,15 +105,20 @@ class MainActivity : ComponentActivity() {
 
         // Solicita los permisos necesarios
         if (permissionsToRequest.isNotEmpty()) {
-            requestPermissions(permissionsToRequest.toTypedArray(), PERMISSION_REQUEST_CODE)
+            requestPermissions(permissionsToRequest.toTypedArray(),
+                PERMISSION_REQUEST_CODE
+            )
         } else {
             // Todos los permisos ya están otorgados, puedes proceder con la captura de la imagen
-            dispatchTakePictureIntent()
         }
     }
 
+
     private fun isPermissionGranted(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(
@@ -155,17 +175,41 @@ class MainActivity : ComponentActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val imageView: ImageView = findViewById(R.id.imageView)
-            imageView.setImageURI(Uri.parse(currentPhotoPath))
+            val photoFile = File(currentPhotoPath)
+            if (photoFile.exists()) {
+                setContent {
+                    ShowPhoto(photoFile.absolutePath)
+                }
+            }
         }
     }
+
+    private fun saveImageToGallery(imageBitmap: Bitmap) {
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "JPEG_${timeStamp}.jpg"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        val imageFile = File(storageDir, imageFileName)
+        val outputStream = FileOutputStream(imageFile)
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        outputStream.flush()
+        outputStream.close()
+
+        // Añade la foto a la galería
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val contentUri = Uri.fromFile(imageFile)
+        mediaScanIntent.data = contentUri
+        sendBroadcast(mediaScanIntent)
+    }
+
 
     @Composable
     fun Greeting(name: String) {
         Text(
             text = "Hello $name!",
             modifier = Modifier.padding(16.dp),
-            color = Color.Transparent,
+            color = Color.White,
             fontWeight = FontWeight.Bold
         )
     }
@@ -173,7 +217,8 @@ class MainActivity : ComponentActivity() {
     @Throws(IOException::class)
     private fun createImageFile(): File {
         // Crea un nombre único para el archivo de imagen
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefijo del archivo */
@@ -186,7 +231,7 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun dispatchTakePictureIntent() {
-        checkAndRequestPermissions()
+        captureImage()
     }
 
     @Composable
@@ -212,6 +257,27 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(16.dp))
                 TakePhotoButton(activity = ComponentActivity())
             }
+        }
+    }
+
+    @Composable
+    fun ShowPhoto(photoPath: String) {
+        val imageBitmap = loadImageBitmap(photoPath)
+        imageBitmap?.let {
+            Image(
+                painter = BitmapPainter(it),
+                contentDescription = null,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+
+    private fun loadImageBitmap(photoPath: String): ImageBitmap? {
+        val photoFile = File(photoPath)
+        return if (photoFile.exists()) {
+            BitmapFactory.decodeFile(photoFile.absolutePath)?.asImageBitmap()
+        } else {
+            null
         }
     }
 }
